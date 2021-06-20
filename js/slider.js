7c9d000241050d42fac394e9d3e6aea594ac6b94
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2021-06-20 23:31:43 +0300

    added slider

---
+++ js/slider.js
@@ -0,0 +1,23 @@
+$(".slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+});
